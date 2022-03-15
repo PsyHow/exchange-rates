412e69ee637930ff467be60a09ef03e7285ec710
PsyHow <viktorburnyshev@gmail.com>
2022-03-15 23:52:56 +0600

    table layout

--- src/components/ValuteList/ValuteList.tsx
+++ src/components/ValuteList/ValuteList.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable react/jsx-key */
 import { ReactElement, useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,18 +8,28 @@ import { fetchExchangeRates } from 'store/middlewares';
 export const ValuteList = (): ReactElement => {
   const dispatch = useDispatch();
   const valuteList = useSelector(selectValuteList);
-  const valuteNameList = valuteList && Object.keys(valuteList);
-
   useEffect(() => {
     dispatch(fetchExchangeRates());
   }, []);
 
-  console.log(valuteList);
-
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      {valuteNameList &&
-        valuteNameList.map((list, index) => <span key={index}>{list}</span>)}
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <td>Валюта</td>
+          <td>Название</td>
+          <td>Стоимость</td>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(valuteList.Valute).map(value => (
+          <tr key={value[1].ID}>
+            <td>{value[0]}</td>
+            <td>{value[1].Name}</td>
+            <td>{value[1].Value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
