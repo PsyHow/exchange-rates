f81d67b27e2ba1125f3613ac9381b4af11e4efcf
PsyHow <viktorburnyshev@gmail.com>
2022-03-21 01:36:14 +0600

    fix

--- src/api/apiConfig/index.ts
+++ src/api/apiConfig/index.ts
@@ -3,5 +3,7 @@ import axios from 'axios';
 export const instance = axios.create({
   baseURL: 'https://www.cbr-xml-daily.ru/',
   withCredentials: true,
-  headers: { 'Access-Control-Allow-Origin': '*' },
+  headers: {
+    'Access-Control-Allow-Origin': 'https://www.cbr-xml-daily.ru/daily_json.js',
+  },
 });
