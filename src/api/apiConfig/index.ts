a1172a45d60041d6a2d7dd2e9028455252b593b7
PsyHow <viktorburnyshev@gmail.com>
2022-03-21 03:04:57 +0600

    fix

--- src/api/apiConfig/index.ts
+++ src/api/apiConfig/index.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: 'https://www.cbr-xml-daily.ru/',
   headers: {
     'Permissions-Policy': 'interest-cohort=()',
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'www.cbr-xml-daily.ru',
   },
 });
