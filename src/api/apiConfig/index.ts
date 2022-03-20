9470a4cce3fd66a8f54209563f699fe8a7ae8298
PsyHow <viktorburnyshev@gmail.com>
2022-03-21 02:57:02 +0600

    try to fix cors error

--- src/api/apiConfig/index.ts
+++ src/api/apiConfig/index.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: 'https://www.cbr-xml-daily.ru/',
-  withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': 'https://www.cbr-xml-daily.ru/daily_json.js',
+    'Permissions-Policy': 'interest-cohort=()',
+    'Access-Control-Allow-Origin': '*',
   },
 });
