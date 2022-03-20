3afac3f2bb525dac0f457c8c70661f663b23b030
PsyHow <viktorburnyshev@gmail.com>
2022-03-21 03:07:29 +0600

    fix

--- src/api/apiConfig/index.ts
+++ src/api/apiConfig/index.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: 'https://www.cbr-xml-daily.ru/',
   headers: {
     'Permissions-Policy': 'interest-cohort=()',
-    'Access-Control-Allow-Origin': 'www.cbr-xml-daily.ru',
+    'Access-Control-Allow-Origin': 'https://psyhow.github.io/exchange-rates/',
   },
 });
