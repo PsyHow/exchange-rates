4f28bef2113733c418566e8f8d2d6c21edc174a1
PsyHow <viktorburnyshev@gmail.com>
2022-03-21 01:31:13 +0600

    add withCredential

--- src/api/apiConfig/index.ts
+++ src/api/apiConfig/index.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: 'https://www.cbr-xml-daily.ru/',
+  withCredentials: true,
   headers: { 'Access-Control-Allow-Origin': '*' },
 });
